7cb92d89251f079ae31c00fc26b40a83f0ed315d
asmo555 <asmo555@166e43a0-c437-0410-82bd-99afdd509fdd>
2009-04-04 15:36:36 +0000

    removed the old data project and replaced it with core and added test project as well

--- trunk/CampaignManager.Core/Properties/AssemblyInfo.cs
+++ trunk/CampaignManager.Core/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Enrick.Games.CampaignManager.Data")]
+[assembly: AssemblyTitle("CampaignManager.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Enrick.Games.CampaignManager.Data")]
-[assembly: AssemblyCopyright("Copyright ©  2007")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("CampaignManager.Core")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e79039f9-ee75-4d48-8f32-9dd50442af69")]
+[assembly: Guid("dfafd4b0-9a97-4ec8-a45e-8db87b25f2f7")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ trunk/CampaignManager.Core/World.cs
@@ -0,0 +1,8 @@
+namespace CampaignManager.Core
+{
+    public class World
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
