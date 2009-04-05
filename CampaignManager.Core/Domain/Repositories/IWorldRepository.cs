326a8761d7ec85dfa9fa9ac77b86940ce3eac7ce
asmo555 <asmo555@166e43a0-c437-0410-82bd-99afdd509fdd>
2009-04-05 03:59:48 +0000

    created an infrastructure project and started stubbing out Repositories with fake data, so tests and a UI can be created. Data will follow along with working Repositories.

--- CampaignManager.Core/Domain/Repositories/IWorldRepository.cs
+++ CampaignManager.Core/Domain/Repositories/IWorldRepository.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
+
 namespace CampaignManager.Core.Domain.Repositories
 {
     public interface IWorldRepository
     {
-        
+        List<World> ListAll();
+        World GetById(int id);
+        bool Save(World world);
     }
 }
